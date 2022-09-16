1fb91542aef6cec17973a7e1fadc30a1b50bf73b
SayedTabish72 <93794214+SayedTabish72@users.noreply.github.com>
2022-09-17 00:39:41 +0530

    Co-authored-by: Sid-d-harth <Sid-d-harth@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import './App.css';
+import React from "react";
+import videoBG from './Assest/videoplayback.mp4';
+
+const Main = () =>{
+    return(
+
+        <div className="main">
+             <div className="overlay"></div>
+        <video src={videoBG} autoPlay loop muted />
+        <div className="content">
+        <h1>LET THE <br></br>GAME BEGIN </h1>
+        <p>hello</p>
+        </div>
+        </div>
+    )
 }
 
-export default App;
+export default Main
