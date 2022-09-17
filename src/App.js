000357e04eaf226f90fdff6bb3323bb28e59855c
SayedTabish72 <93794214+SayedTabish72@users.noreply.github.com>
2022-09-17 23:40:01 +0530

    Merge branch 'master' of https://github.com/Sid-d-harth/Game-Portal
    Co-authored-by: Sid-d-harth <Sid-d-harth@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
-
 // import './App.css';
 import React from "react";
-import videoBG from './Assest/videoplayback.mp4';
+import videoBG from "./Assest/videoplayback.mp4";
+import tamplate from "./Assest/Blue and Red 72x36 Landscape Campaign Banner.png";
+import tamplate2 from "./Assest/imge2.png";
+import { FooterContainer } from "./HomePage/containers/footer";
 
-const Main = () =>{
-    return(
+const Main = () => {
+  return (
+    // Creating view-point for landing pape
+    <div className="main">
+      <div className="overlay"></div>
+      <video src={videoBG} autoPlay loop muted />
+      <div className="content">
+        <h1>
+          LET THE <br></br>GAME BEGIN
+        </h1>
+        <p>
+          An indian digital online games and entertainment site.<br></br> We
+          develop, publish and distribute multiplayer games.
+        </p>
+      </div>
 
-        <div className="main">
-             <div className="overlay"></div>
-        <video src={videoBG} autoPlay loop muted />
-        <div className="content">
-        <h1>LET THE <br></br>GAME BEGIN </h1>
-        <p>hello</p>
+      <div className="aboutsection">
+        <img src={tamplate} />
+        <div className="sitecontent">
+          <h2>
+            We're Developing The Site Which alow's the user <br></br>To play
+            there fav game online without Dyarownloading{" "}
+          </h2>
         </div>
-        </div>
-    )
-}
 
-export default Main
+        <div className="aboutsection2">
+          <img src={tamplate2} />
+          <div className="sitecontent"></div>
+        </div>
+
+        {/* for footer */}
+        <>
+          <FooterContainer />
+        </>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
